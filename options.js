0d11762ff488eda96a8d5f663c16183fbb0561cf
Komi Serge Innocent <komi.innocent@gmail.com>
2016-02-04 16:22:24 +0000

    porter le blog sur ghost

---
+++ options.js
@@ -0,0 +1,28 @@
+/**
+ * Dbyll Theme Options
+ */
+
+var site = {
+  
+  sidebar: {
+    textColor: '#fff',
+    backgroundColor: '#2c3e50',
+  },
+
+  headerColor: '#e74c3c',
+
+  author: {
+    email: "komi.innocent@gmail.com",
+    github: "centonni",
+    twitter: "centonni",
+    pinterest: "",
+    linkedin: ""
+  },
+
+  disqus: {
+    shortname: 'centonni'
+  },
+
+  analyticsTrackId: 'UA-50638672-1'
+  
+}
